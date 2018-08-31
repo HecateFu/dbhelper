920b39beea1e97b13dfd94748277b654eb7663c9
HecateFu <hecatefu@outlook.com>
2018-08-31 13:37:39 +0800

    init

---
+++ src/main/java/org/fcx/dbhelper/App.java
@@ -0,0 +1,13 @@
+package org.fcx.dbhelper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
