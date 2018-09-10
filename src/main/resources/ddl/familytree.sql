4a4fe77f11dd10c95cc9cc4fd0e57c36861e91ef
HecateFu <hecatefu@outlook.com>
2018-09-10 14:28:44 +0800

    familytree-ddl

--- src/main/resources/ddl/familytree.sql
+++ src/main/resources/ddl/familytree.sql
@@ -25,9 +25,12 @@ create table familytree.person (
     key idx_person(name)
 ) engine=InnoDB default charset = 'utf8mb4' comment='人员信息';
 
-create table familytree.person (
+create table familytree.person_relation (
 	id int auto_increment comment '主键，自增',
-    
+    person_id int not null comment '人员id,person.id',
+    father_id int not null comment '父亲id,person.id',
+    mother_id int not null comment '母亲id,person.id',
+    partner_id int not null comment '配偶id,person.id',
     create_date datetime default current_timestamp comment '创建日期',
     create_by int comment '创建人,person.id',
     update_date datetime default current_timestamp on update current_timestamp comment '修改日期',
@@ -36,6 +39,14 @@ create table familytree.person (
 ) engine=InnoDB default charset = 'utf8mb4' comment='人员关系';
 
 
-INSERT INTO comment_tree_paths(ancestor,descendant) 
-SELECT t.ancestor,@NEW_COMMENT_ID FROM comment_tree_paths as t WHERE t.descendant = 1
-UNION ALL SELECT @NEW_COMMENT_ID,@NEW_COMMENT_ID;
+create table familytree.contact_way (
+	id int auto_increment comment '主键，自增',
+    person_id int not null comment '人员id,person.id',
+    contact_type int not null comment '联系方式类型 0手机 1微信号 2QQ 3email 4固话 5新浪微博 6facebook 7twitter',
+    number varchar(30) not null comment '联系方式号码',
+    create_date datetime default current_timestamp comment '创建日期',
+    create_by int comment '创建人,person.id',
+    update_date datetime default current_timestamp on update current_timestamp comment '修改日期',
+    update_by int comment '修改人,person.id',
+    primary key(id)
+) engine=InnoDB default charset = 'utf8mb4' comment='联系方式';
